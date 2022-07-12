55a3001f17b361dc436a1497abbd12eb3bb7ab20
rasma123 <82595793+rasma123@users.noreply.github.com>
2022-07-12 19:50:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+
+Webcam.set ({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach ('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+    document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">';
+
+    });
+}
+
+console.log('ml5 version:',ml5.version);
+classifier =ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3c6_RSM7f/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log('model Loaded!');
+}
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML =  'object : ' + results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML ='accuray : ' + ( results[0].confidence * 100).toFixed(1);
+
+    }
+}
